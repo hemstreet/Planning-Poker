cd0afb5c97a0518f343e54ffa62a296e81cc9187
Justin Walk <jwalk@arrow.com>
2016-02-09 14:43:45 -0700

    started materialization!!

--- app/app.js
+++ app/app.js
@@ -1,20 +1,26 @@
 "use strict";
 
-var app = angular.module('planningPoker', ['ngRoute', 'btford.socket-io']).factory('socket', function (socketFactory) {
+var app = angular.module('planningPoker', ['ngRoute', 'btford.socket-io', 'ngMaterial'])
+  .factory('socket', function(socketFactory) {
     return socketFactory();
-});
+  })
+  .config(function($mdThemingProvider){
+    $mdThemingProvider.theme('default')
+      .primaryPalette('blue')
+      .accentPalette('blue-grey');
+  });
 
-app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/', {
-        templateUrl: 'views/home.html',
-        controller: 'HomeController',
-        controllerAs: 'vm'
-    }).when('/room/:roomId', {
-        templateUrl: 'views/room.html',
-        controller: 'RoomController',
-        controllerAs: 'vm'
-    }).otherwise({
-        redirectTo: '/'
-    });
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl:'views/home.html',
+    controller:'HomeController',
+    controllerAs:'vm'
+  }).when('/room/:roomId', {
+    templateUrl:'views/room.html',
+    controller:'RoomController',
+    controllerAs:'vm'
+  }).otherwise({
+    redirectTo:'/'
+  });
 
 }]);
